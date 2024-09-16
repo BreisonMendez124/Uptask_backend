0ba365445ba062cade8012c6371b8e7b1505092d
BreisonMendezServi <breison.mendez@servinformacion.com>
2024-09-16 12:59:59 -0500

    Creacion de Estructua de proyecto y Metodo POST Se crea la estructura MVC Creacion de rutas de guardado de proyectos Creacion de la funcion de creacion de proyectos para la API

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import  express from "express";
 import dotenv from 'dotenv'
 import { connectDB } from "./config/db";
+import ProjectRoutes from './routes/ProjectRoutes'
 
 
 dotenv.config()
 connectDB();
+
 const app = express()
+app.use( express.json() )
+
+//Routes
+app.use('/api/projects' , ProjectRoutes )
 export default app
