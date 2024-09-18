2a6d807d2c7986f9822a99dfce6f303e20c04232
BreisonMendezServi <breison.mendez@servinformacion.com>
2024-09-18 12:59:59 -0500

    Creacion de modelos de tareas y Ajuste de modelo de Proyectos

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,9 +1,22 @@
-import mongoose, { Document, Schema } from "mongoose";
+import mongoose, { Document, PopulatedDoc, Schema, Types } from "mongoose";
+import { ITask } from "./Task";
 
-export type ProjectType = Document & { 
+const taskStatus = { 
+    PENDING: "pending",
+    ON_HOLD: "onHold",
+    IN_PROGRESS: "inProgress",
+    UNDER_REVIEW: "underReview",
+    COMPLETED: "completed"
+}
+
+export type TaskStatus = typeof taskStatus[ keyof typeof taskStatus]
+
+export interface IProject extends Document { 
     projectName: string
     clientName: string
     description: string
+    tasks: PopulatedDoc<ITask & Document>[],
+    status: TaskStatus
 }
 
 const ProjectSchema: Schema = new Schema( { 
@@ -21,8 +34,17 @@ const ProjectSchema: Schema = new Schema( {
         type: String,
         required: true,
         trim: true
+    },
+    tasks: [ {
+            type: Types.ObjectId,
+            ref: "Task" 
+    }],
+    status: { 
+        type: String,
+        enum: Object.values( taskStatus ),
+        default: taskStatus.PENDING
     }
-})
+} , { timestamps: true } )
 
-const Project = mongoose.model<ProjectType>('Project' , ProjectSchema );
+const Project = mongoose.model<IProject>('Project' , ProjectSchema );
 export default Project;
